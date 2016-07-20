f963e493287f1bfb1a21215b486c3c1559661a6a
John Andersen <john.s.andersen@intel.com>
2016-07-20 12:49:25 -0700

    Reads from binary outputs JSON
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,55 @@
-#include <cereal/types/unordered_map.hpp>
-#include <cereal/types/memory.hpp>
+#include <cereal/types/string.hpp>
 #include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
 #include <fstream>
+#include <iostream>
 
-struct MyRecord
+struct Minimal
 {
-  uint8_t x, y;
-  float z;
+  std::string myData;
+
+  template <class Archive>
+  std::string save_minimal( Archive const & ) const
+  { return myData; }
+
+  template <class Archive>
+  void load_minimal( Archive const &, std::string const & value )
+  { myData = value; }
+};
+
+struct Normal
+{
+  std::string myData;
 
   template <class Archive>
   void serialize( Archive & ar )
-  {
-    ar( x, y, z );
-  }
-};
-
-struct SomeData
-{
-  int32_t id;
-  std::shared_ptr<std::unordered_map<uint32_t, MyRecord>> data;
-
-  template <class Archive>
-  void save( Archive & ar ) const
-  {
-    ar( data );
-  }
-
-  template <class Archive>
-  void load( Archive & ar )
-  {
-    static int32_t idGen = 0;
-    id = idGen++;
-    ar( data );
-  }
+  { ar( myData ); }
 };
 
 int main()
 {
-  std::ofstream os("out.cereal", std::ios::binary);
-  cereal::BinaryOutputArchive archive( os );
+    /*
+    {
+        Minimal m = {"minimal"};
+        Normal  n = {"normal"};
 
-  SomeData myData;
-  archive( myData );
+        std::ofstream os("data.xml");
+        cereal::BinaryOutputArchive oar(os);
+        oar( CEREAL_NVP(m), CEREAL_NVP(n) );
+    }
+    */
 
-  return 0;
+    {
+        Minimal m;
+        Normal  n;
+
+        // std::ifstream is("data.xml");
+        // cereal::BinaryInputArchive iar(is);
+        cereal::BinaryInputArchive iar(std::cin);
+        iar(m, n);
+
+        cereal::JSONOutputArchive oar(std::cout);
+        oar( CEREAL_NVP(m), CEREAL_NVP(n) );
+    }
 }
+
